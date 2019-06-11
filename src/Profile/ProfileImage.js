07560f7829077d5bd88cfe682ba43db97d9ed424
josephprasanth <prasanthjoseph_77@yahoo.com>
2019-06-11 21:08:22 +0200

    Adding ProfileImage.js for top section

---
+++ src/Profile/ProfileImage.js
@@ -0,0 +1,8 @@
+import styled, {css} from 'styled-components';
+
+export const ProfileImage = styled.div`
+width: 200px;
+height: 200px;
+background: no-repeat center/170% url(/img/mlogo.png);
+margin: 40px;
+`
